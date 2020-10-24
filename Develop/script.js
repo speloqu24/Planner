2dddce0e97243609ce98f30c36ad9653a2000e26
Speloquin <speloqu24@gmail.com>
2020-10-24 15:41:12 -0600

    redirected the code, to do more creation with HTML

--- Develop/script.js
+++ Develop/script.js
@@ -3,72 +3,124 @@
 var today = moment().format("LLLL");
 // var now = moment().format("HH:MM")
 document.getElementById("currentDay").innerHTML = today;
-// document.getElementById("currentDay").innerHTML = today;
-
-var hourDiv = document.getElementById("agenda");
-
-var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM" ];
-
-for (var i = 0; i < hours.length; i++) {
-  var hourDisplay = document.createElement("div");
-  var txtDisplay = document.createElement("textarea");
-  var button = document.createElement("button");
-
-  hourDisplay.textContent = hours[i];
-  hourDisplay.setAttribute("class", "row col-md-1 hour");
-  hourDisplay.setAttribute("id", "hoursLoop");
 
 
-  txtDisplay.setAttribute("class", "col-md-10 description");
-  txtDisplay.setAttribute("id", "txtDescription")
+var task9 = document.getElementById("btn9");
+var taskTxt9 = document.getElementById("task9");
 
-  
-  button.textContent = "SAVE";
-  button.setAttribute("class", "saveBtn col-md-1");
-  button.setAttribute("id", "button" )
+task9.addEventListener("click", function (event){
+event.preventDefault();
+var nineAM = taskTxt9.value
+localStorage.setItem("nineAM", nineAM);
+})
 
-  // txtDisplay.classList.add("textarea");
-
-  hourDiv.append(hourDisplay);
-  hourDiv.append(txtDisplay);
-  hourDiv.append(button);
-}
+var planner9get = localStorage.getItem("nineAM");
+taskTxt9.textContent = planner9get;
 
 
-// function localStorage ( ){ 
-// var hourID = document.getElementById("hoursLoop");
+var task10 = document.getElementById("btn10");
+var taskTxt10 = document.getElementById("task10");
 
-// var textInput = document.querySelectorAll(".description");
+task10.addEventListener("click", function (event){
+event.preventDefault();
+var tenAM = taskTxt10.value
+localStorage.setItem("tenAM", tenAM);
+})
 
-// var textInputLocalStorage = textInput.value;
-// localStorage.setItem(hourID, textInputLocalStorage);
-
-// // }
-
-// var saveTask = document.getElementById("button");
-
-$(".saveBtn").on("click", function()
-
-)
+var planner10get = localStorage.getItem("tenAM");
+taskTxt10.textContent = planner10get;
 
 
-// saveTask.addEventListener("click", function (event){
-//     event.preventDefault();
-//     console.log("clicked")
-// }
-//   var textInput = document.querySelectorAll(".description");
-//   // var hourID = document.getElementById("hoursLoop");
+// 11
 
-//   for (var i=0; i< textInput.length; i++) {
-//   console.log(textInput[i].value);
+var task11 = document.getElementById("btn11");
+var taskTxt11 = document.getElementById("task11");
 
-//   var textInputLocalStorage = textInput[i].value;
-//   localStorage.setItem("to do", textInputLocalStorage);
+task11.addEventListener("click", function (event){
+event.preventDefault();
+var elevenAM = taskTxt11.value
+localStorage.setItem("elevenAM", elevenAM);
+})
 
-// }
-// })
+var planner11get = localStorage.getItem("elevenAM");
+taskTxt11.textContent = planner11get;
 
-// var textInput = document.getElementById("txtDescription")
+// 12
 
-// var taskEntry = localStorage.getItem("newTask")
-// textInput.textContent = taskEntry;
+var task12 = document.getElementById("btn12");
+var taskTxt12 = document.getElementById("task12");
+
+task12.addEventListener("click", function (event){
+event.preventDefault();
+var twelvePM = taskTxt12.value
+localStorage.setItem("twelvePM", twelvePM);
+})
+
+var planner12get = localStorage.getItem("twelvePM");
+taskTxt12.textContent = planner12get;
+
+
+// 1
+var task1 = document.getElementById("btn1");
+var taskTxt1 = document.getElementById("task1");
+
+task1.addEventListener("click", function (event){
+event.preventDefault();
+var onePM = taskTxt1.value
+localStorage.setItem("onePM", onePM);
+})
+
+var planner1get = localStorage.getItem("onePM");
+taskTxt1.textContent = planner1get;
+
+// 2
+var task2 = document.getElementById("btn2");
+var taskTxt2 = document.getElementById("task2");
+
+task2.addEventListener("click", function (event){
+event.preventDefault();
+var twoPM = taskTxt2.value
+localStorage.setItem("twoPM", twoPM);
+})
+
+var planner2get = localStorage.getItem("twoPM");
+taskTxt2.textContent = planner2get;
+
+// 3
+var task3 = document.getElementById("btn3");
+var taskTxt3 = document.getElementById("task3");
+
+task3.addEventListener("click", function (event){
+event.preventDefault();
+var threePM = taskTxt3.value
+localStorage.setItem("threePM", threePM);
+})
+
+var planner3get = localStorage.getItem("threePM");
+taskTxt3.textContent = planner3get;
+
+// 4
+var task4 = document.getElementById("btn4");
+var taskTxt4 = document.getElementById("task4");
+
+task4.addEventListener("click", function (event){
+event.preventDefault();
+var fourPM = taskTxt4.value
+localStorage.setItem("fourPM", fourPM);
+})
+
+var planner4get = localStorage.getItem("fourPM");
+taskTxt4.textContent = planner4get;
+
+// 5
+var task5 = document.getElementById("btn5");
+var taskTxt5 = document.getElementById("task5");
+
+task5.addEventListener("click", function (event){
+event.preventDefault();
+var fivePM = taskTxt5.value
+localStorage.setItem("fivePM", fivePM);
+})
+
+var planner5get = localStorage.getItem("fivePM");
+taskTxt5.textContent = planner5get;
