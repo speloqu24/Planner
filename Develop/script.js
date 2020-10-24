2d79e1587ac1e6a5470bb6b060352d5a2af8439a
Speloquin <speloqu24@gmail.com>
2020-10-23 19:25:02 -0600

    local storage and ID

--- Develop/script.js
+++ Develop/script.js
@@ -12,37 +12,44 @@ var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM" ];
 for (var i = 0; i < hours.length; i++) {
   var hourDisplay = document.createElement("div");
   var txtDisplay = document.createElement("textarea");
-  var button = document.createElement("button")
+  var button = document.createElement("button");
 
   hourDisplay.textContent = hours[i];
-  hourDisplay.setAttribute("class", "row col-md-1 hour")
+  hourDisplay.setAttribute("class", "row col-md-1 hour");
+  hourDisplay.setAttribute("id", "hoursLoop");
 
 
 
-  txtDisplay.setAttribute("class", "col-md-10 description")
+  txtDisplay.setAttribute("class", "col-md-10 description");
 
   
-  button.textContent = "SAVE"
-  button.setAttribute("class", "saveBtn col-md-1")
+  button.textContent = "SAVE";
+  button.setAttribute("class", "saveBtn col-md-1");
 
   // txtDisplay.classList.add("textarea");
 
   hourDiv.append(hourDisplay);
   hourDiv.append(txtDisplay);
-  hourDiv.append(button)
+  hourDiv.append(button);
+}
+
+function localStorage ( ){ 
+var hourID = document.getElementById("hoursLoop");
+
+var textInput = document.querySelectorAll(".description");
+
+var textInputLocalStorage = textInput.value;
+localStorage.setItem(hourID, textInputLocalStorage);
+
 }
 
 var saveTask = document.querySelector(".saveBtn");
-var textInput = document.querySelector(".description")
 
 saveTask.addEventListener("click", function (event){
     event.preventDefault();
-    console.log("I've been clicked")
-    var newTask = textInput.value
-    localStorage.setItem("To do", newTask)
-
+    console.log("clicked")
+    localStorage()
 })
 
-// var taskEntry = localStorage.getItem("newTask);
-// textInpu.textContent = taskEntry;
-
+// var taskEntry = localStorage.getItem("newTask")
+// textInput.textContent = taskEntry;
